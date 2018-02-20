6e4d2d7ffc92fe6576289c11ff50b2015f8cc35d
Marla Foreman <marlaforeman@marlas-mbp.lan>
2018-02-19 22:03:59 -0600

    Adding chess pawns to the board

--- src/app/chess.service.ts
+++ src/app/chess.service.ts
@@ -1,6 +1,7 @@
 import { Injectable }           from '@angular/core';
 import { CheckerBoard }	        from './checkerBoard';
 import { Space }                from './space';
+import { Piece, chessPawn }		from './piece';
 
 
 @Injectable()
@@ -14,5 +15,10 @@ export class ChessService {
 	// Resets game back to beginning
     resetGame() {
     	this.board = new CheckerBoard().board;
+    	// Adding pawns
+    	for (let j = 0; j < 8; j++) {
+    		this.board[1][j].addPiece(new chessPawn('red', 1, j));
+    		this.board[6][j].addPiece(new chessPawn('black', 1, j));
+    	}
     }
 }
