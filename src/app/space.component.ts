20c87f5f0a5d16fa8d323966d75486bf8fca5b7a
Marla Foreman <marlaforeman@Marlas-MacBook-Pro.local>
2018-02-26 14:07:40 -0600

    Getting king animation set up on space component

--- src/app/space.component.ts
+++ src/app/space.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input }	from '@angular/core';
-import { Space, chessSpace }			from './space';
+import { Space, chessSpace }from './space';
 import { CheckersService }	from './checkers.service';
 import { ChessService }		from './chess.service';
 import { trigger, state, style, animate, transition, animateChild }	from '@angular/animations';
@@ -8,7 +8,15 @@ import { trigger, state, style, animate, transition, animateChild }	from '@angul
   	selector: 'space',
   	templateUrl: './space.component.html',
   	styleUrls: ['./space.component.css'],
-  	animations: []
+  	animations: [
+  		trigger('kingAnimation', [
+      		state('true',   style({
+        		backgroundColor: '#FF0000',
+      		})),
+	      	transition('false => true', animate('100ms ease-in')),
+	      	transition('true => false', animate('100ms ease-out'))
+	    ])
+  	]
 })
 export class SpaceComponent {
 	@Input() space: chessSpace;
@@ -17,4 +25,5 @@ export class SpaceComponent {
   		private checkers: CheckersService,
   		private chess: ChessService
   	) {}
+
 }
