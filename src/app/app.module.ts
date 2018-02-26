b9e407b95a121635b320db9e83eadb4800b4c16c
Marla Foreman <marlaforeman@Marlas-MacBook-Pro.local>
2018-02-26 13:35:53 -0600

    Starting adding in animations to flash red when king is in check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule }         from '@angular/platform-browser';
-import { NgModule }              from '@angular/core';
+// Module Imports
+import { BrowserModule }              from '@angular/platform-browser';
+import { NgModule }                   from '@angular/core';
+import { BrowserAnimationsModule }    from '@angular/platform-browser/animations'
 
 // Shared
-import { AppComponent }          from './app.component';
-import { GameBoardComponent }    from './game-board.component';
-import { GameConsoleComponent }  from './game-console.component';
-import { SpaceComponent }        from './space.component';
+import { AppComponent }               from './app.component';
+import { GameBoardComponent }         from './game-board.component';
+import { GameConsoleComponent }       from './game-console.component';
+import { SpaceComponent }             from './space.component';
 
 // Checkers Only
-import { CheckersService }       from './checkers.service';
-import { PawnComponent }         from './pieces/pawn.component';
-import { KingComponent }         from './pieces/king.component';
+import { CheckersService }            from './checkers.service';
+import { PawnComponent }              from './pieces/pawn.component';
+import { KingComponent }              from './pieces/king.component';
 
 // Chess Only
-import { ChessService }          from './chess.service';
-import { ChessPieceComponent }    from './pieces/chessPiece.component';
+import { ChessService }               from './chess.service';
+import { ChessPieceComponent }        from './pieces/chessPiece.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,8 @@ import { ChessPieceComponent }    from './pieces/chessPiece.component';
     ChessPieceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [
   	CheckersService,
--- src/app/space.component.ts
+++ src/app/space.component.ts
@@ -2,11 +2,13 @@ import { Component, Input }	from '@angular/core';
 import { Space }			from './space';
 import { CheckersService }	from './checkers.service';
 import { ChessService }		from './chess.service';
+import { trigger, state, style, animate, transition, animateChild }	from '@angular/animations';
 
 @Component({
   	selector: 'space',
   	templateUrl: './space.component.html',
   	styleUrls: ['./space.component.css'],
+  	animations: []
 })
 export class SpaceComponent {
 	@Input() space: Space;
