0807f41b219dfd8920492405f1155e1e4508fada
Marla Foreman <marla294@gmail.com>
2018-04-19 11:53:37 -0500

    Some styling stuff that I forgot to check in a while ago
    Womp womp

--- src/app/game-board.component.ts
+++ src/app/game-board.component.ts
@@ -1,31 +1,31 @@
 // This will be a component that houses the actual checkers game board
-import { Component, Input }	     from '@angular/core';
-import { OnInit } 		           from '@angular/core';
-import { Piece }	 	             from './pieces/piece';
-import { CheckersService }       from './checkers.service';
-import { ChessService }          from './chess.service';
-import { Observable }            from 'rxjs/Observable';
-import { SpaceComponent }        from './space.component';
+import { Component, Input } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { Piece } from "./pieces/piece";
+import { CheckersService } from "./checkers.service";
+import { ChessService } from "./chess.service";
+import { Observable } from "rxjs/Observable";
+import { SpaceComponent } from "./space.component";
 
 @Component({
-  selector: 'game-board',
-  templateUrl: './game-board.component.html',
-  styleUrls: ['./game-board.component.css'],
+    selector: "game-board",
+    templateUrl: "./game-board.component.html",
+    styleUrls: ["./game-board.component.css"]
 })
 export class GameBoardComponent implements OnInit {
     @Input() chessOrCheckers;
-	  public board: any;
+    public board: any;
 
     // Observables
     public resetGame$: Observable<boolean>;
 
-  	constructor(
-  		private checkers: CheckersService,
+    constructor(
+        private checkers: CheckersService,
         private chess: ChessService
-  	) {}
+    ) {}
 
-  	ngOnInit() {
-        if (this.chessOrCheckers === 'checkers') {
+    ngOnInit() {
+        if (this.chessOrCheckers === "checkers") {
             //Observables
             this.resetGame$ = this.checkers.resetGameObs;
             this.resetGame$.subscribe(reset => {
@@ -36,7 +36,7 @@ export class GameBoardComponent implements OnInit {
 
             // Always reset game on init anyway
             this.onResetCheckers();
-        } else if (this.chessOrCheckers === 'chess') {
+        } else if (this.chessOrCheckers === "chess") {
             //Observables
             this.resetGame$ = this.chess.resetGameObs;
             this.resetGame$.subscribe(reset => {
@@ -48,8 +48,9 @@ export class GameBoardComponent implements OnInit {
             // Always reset game on init anyway
             this.onResetChess();
         }
-        
-  	}
+
+        console.log(window.innerHeight);
+    }
 
     onResetCheckers() {
         this.checkers.resetGame();
--- src/app/space.component.ts
+++ src/app/space.component.ts
@@ -1,30 +1,40 @@
-import { Component, Input }	from '@angular/core';
-import { Space, chessSpace }from './space';
-import { CheckersService }	from './checkers.service';
-import { ChessService }		from './chess.service';
-import { trigger, state, style, animate, transition, animateChild }	from '@angular/animations';
+import { Component, Input } from "@angular/core";
+import { Space, chessSpace } from "./space";
+import { CheckersService } from "./checkers.service";
+import { ChessService } from "./chess.service";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  animateChild
+} from "@angular/animations";
 
 @Component({
-  	selector: 'space',
-  	templateUrl: './space.component.html',
-  	styleUrls: ['./space.component.css'],
-  	animations: [
-  		trigger('kingAnimation', [
-      		state('true',   style({
-        		backgroundColor: '#FF0000',
-      		})),
-	      	transition('false => true', animate('100ms ease-in')),
-	      	transition('true => false', animate('100ms ease-out'))
-	    ])
-  	]
+  selector: "space",
+  templateUrl: "./space.component.html",
+  styleUrls: ["./space.component.css"],
+  animations: [
+    trigger("kingAnimation", [
+      state(
+        "true",
+        style({
+          backgroundColor: "#FF0000"
+        })
+      ),
+      transition("false => true", animate("100ms ease-in")),
+      transition("true => false", animate("100ms ease-out"))
+    ])
+  ]
 })
 export class SpaceComponent {
-	@Input() space: chessSpace;
+  @Input() space: chessSpace;
   @Input() chessOrCheckers: string;
 
-	constructor(
-  		private checkers: CheckersService,
-  		private chess: ChessService
-  	) {}
+  constructor(private checkers: CheckersService, private chess: ChessService) {}
 
+  onResize(event) {
+    console.log(event);
+  }
 }
